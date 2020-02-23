60ed053b340c0b2b8e8d0b5ce9f5e698fd754644
Azat <kaumov.a.r@gmail.com>
2020-02-23 21:08:02 +0300

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Azat Kaiumov on 23.02.2020.
+//  Copyright © 2020 Azat Kaiumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
